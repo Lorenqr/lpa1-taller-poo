633245c701ad3b61b342e625786995cb8e71714f
lorenqr <lorenagr223@gmail.com>
2025-03-21 20:02:03 -0500

    Modificacion muebler.py

---
+++ muebleria/mueble.py
@@ -0,0 +1,16 @@
+# clases relacionadas con los muebles
+
+from abc import ABC, abstractmethod
+
+class Mueble(ABC):
+    def __init__(self, material, precio, modelo):
+        self.material = material
+        self.precio = precio
+        self.modelo = modelo
+
+    @abstractmethod
+    def calcular_precio_final(self):
+        pass
+
+    def __str__(self):
+        return f"{self.__class__.__name__} - Modelo: {self.modelo}, Material: {self.material}, Precio: ${self.precio:.2f}"
